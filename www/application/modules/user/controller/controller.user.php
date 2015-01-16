b841a282f44c958d40df48c47ba294034022d163
Dmitriy Shamrik <shamrik.dmitriy@gmail.com>
2015-01-16 21:57:07 +0300

    ...
    ...

--- www/application/modules/user/controller/controller.user.php
+++ www/application/modules/user/controller/controller.user.php
@@ -5,3 +5,15 @@
  * Date: 16.01.2015
  * Time: 21:51
  */
+class UserController{
+
+    private static $_instance = null;
+
+    public function __construct(){
+
+    }
+
+    public function accountInfo(){
+        
+    }
+}
